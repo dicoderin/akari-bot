7a5f8a8450729eecfb741eb3061a2f3e5aafdab7
Lynx <159797095+dicoderin@users.noreply.github.com>
2024-03-11 13:12:42 +0700

    Update bot.py
    corrected with a more detailed explanation

--- bots/aiogram/bot.py
+++ bots/aiogram/bot.py
@@ -1,3 +1,4 @@
+# Importasi modul yang diperlukan
 import asyncio
 import os
 import sys
@@ -14,35 +15,57 @@ from core.types import MsgInfo, Session
 from core.utils.bot import load_prompt, init_async
 from core.utils.info import Info
 
+# Mengatur aset pribadi
 PrivateAssets.set(os.path.abspath(os.path.dirname(__file__) + '/assets'))
+
+# Menonaktifkan mm di Url
 Url.disable_mm = True
 
-
+# Mendefinisikan handler pesan
 @dp.message()
 async def msg_handler(message: types.Message):
+    # Mendefinisikan target_id dan reply_id
     target_id = f'Telegram|{message.chat.type}|{message.chat.id}'
     reply_id = None
+
+    # Memeriksa apakah pesan adalah balasan
     if message.reply_to_message:
         reply_id = message.reply_to_message.message_id
-    msg = MessageSession(MsgInfo(target_id=target_id,
-                                 sender_id=f'Telegram|User|{message.from_user.id}',
-                                 target_from=f'Telegram|{message.chat.type}',
-                                 sender_from='Telegram|User', sender_name=message.from_user.username,
-                                 client_name=client_name,
-                                 message_id=message.message_id,
-                                 reply_id=reply_id),
-                         Session(message=message, target=message.chat.id, sender=message.from_user.id))
+
+    # Membuat MessageSession
+    msg = MessageSession(
+        MsgInfo(
+            target_id=target_id,
+            sender_id=f'Telegram|User|{message.from_user.id}',
+            target_from=f'Telegram|{message.chat.type}',
+            sender_from='Telegram|User',
+            sender_name=message.from_user.username,
+            client_name=client_name,
+            message_id=message.message_id,
+            reply_id=reply_id
+        ),
+        Session(
+            message=message,
+            target=message.chat.id,
+            sender=message.from_user.id
+        )
+    )
+
+    # Mem-parsing pesan
     await parser(msg)
 
-
+# Mendefinisikan fungsi on_startup
 async def on_startup(dispatcher):
+    # Menginisialisasi dan memuat prompt
     await init_async()
     await load_prompt(FetchTarget)
 
-
+# Memeriksa apakah 'subprocess' ada di sys.argv
 if 'subprocess' in sys.argv:
     Info.subprocess = True
 
+# Mendaftarkan startup
 dp.startup.register(on_startup)
 
+# Memulai polling
 asyncio.run(dp.start_polling(bot))
